586f751282a5e220afd1ea309accced2fce58ed8
kvukovic4 <katarina.vukovic3@gmail.com>
2022-01-07 09:28:09 +0100

    next and react project init

---
+++ next/pages/_app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import createEmotionCache from '../utility/createEmotionCache';
+import theme from '../styles/theme/theme';
+import '../styles/globals.css';
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props) => {
+	const {
+		Component,
+		emotionCache = clientSideEmotionCache,
+		pageProps,
+		categories,
+	} = props;
+	const getLayout = Component.getLayout || ((page) => page);
+	return (
+		<CacheProvider value={emotionCache}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				{getLayout(<Component {...pageProps} />)}
+			</ThemeProvider>
+		</CacheProvider>
+	);
+};
+
+export default MyApp;
